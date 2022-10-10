3bab784f1c06433ffefa073a6c7695abd2eb2be7
Allan Jardine <allan@datatables.net>
2022-10-10 19:50:49 +0000

    Add support for mjs files (ES Modules)

---
+++ js/scroller.bulma.min.mjs
@@ -0,0 +1,1 @@
+import $ from"jquery";import DataTable from"datatables.net-bm";import DataTable from"datatables.net-scroller";export default DataTable;
---
+++ js/scroller.bulma.mjs
@@ -0,0 +1,13 @@
+
+/*! Bulma styling wrapper for Scroller
+ * © SpryMedia Ltd - datatables.net/license
+ */
+
+import $ from 'jquery';
+import DataTable from 'datatables.net-bm';
+import DataTable from 'datatables.net-scroller';
+
+
+
+
+export default DataTable;
